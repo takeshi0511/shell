141cc0edc6d71c311d2d51b6c0a91a181b782c18
ts03511 <ts03511@gmail.com>
2020-01-12 13:15:17 +0900

    powershellのコア部分を実装

---
+++ windows_scripts/CreatePassword.ps1
@@ -0,0 +1,11 @@
+﻿# Password Management Tool
+
+# Load Library from .Net Framework
+Add-Type -Assembly System.Web
+
+# Create and Encrypt Password
+$Plain_Password_String=[System.Web.Security.Membership]::GeneratePassword(20,7)
+$Secure_Password_String = ConvertTo-SecureString $Plain_Password_String -AsPlainText -Force
+$Encrypted_Password_String = ConvertFrom-SecureString -SecureString $Secure_Password_String
+
+Set-Content $env:USERPROFILE\Documents\Password\Password.txt -Value $Encrypted_Password_String
